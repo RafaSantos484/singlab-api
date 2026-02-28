2c566004652e8e8c8ea4f8d1b6f9aed6ff3b954c
rafael.santos <rafaelsantos32141@gmail.com>
2026-02-28 17:18:28 -0300

    refactor: update separations module imports after provider reorganization

--- src/features/separations/index.ts
+++ src/features/separations/index.ts
@@ -1,6 +1,6 @@
 export { SeparationsModule } from './separations.module';
 export { SeparationsController } from './separations.controller';
 export { SeparationsService } from './separations.service';
-export * from './providers/separation-provider.types';
+export * from './providers/poyo/poyo-separation.types';
 export { StemSeparationProviderFactory } from './providers/stem-separation-provider.factory';
-export { PoyoStemSeparationProvider } from './providers/poyo-separation.provider';
+export { PoyoStemSeparationProvider } from './providers/poyo/poyo-separation.provider';
--- src/features/separations/separations.module.ts
+++ src/features/separations/separations.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { SeparationsController } from './separations.controller';
 import { SeparationsService } from './separations.service';
 import { StemSeparationProviderFactory } from './providers/stem-separation-provider.factory';
-import { PoyoStemSeparationProvider } from './providers/poyo-separation.provider';
+import { PoyoStemSeparationProvider } from './providers/poyo/poyo-separation.provider';
 import { FirebaseAdminProvider } from '../../auth/firebase-admin.provider';
 import { SongsModule } from '../songs/songs.module';
 
