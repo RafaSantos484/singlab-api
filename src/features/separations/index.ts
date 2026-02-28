baff374afb79d684f39af2415614b13861636474
rafael.santos <rafaelsantos32141@gmail.com>
2026-02-28 13:50:08 -0300

    chore: remove unused separation types and DTOs
    Remove obsolete types from separation-provider.types.ts (enums, interfaces, helper functions). Delete SubmitSeparationDto. Remove express.d.ts type augmentation. Clean up index exports.

--- src/features/separations/index.ts
+++ src/features/separations/index.ts
@@ -1,7 +1,6 @@
 export { SeparationsModule } from './separations.module';
 export { SeparationsController } from './separations.controller';
 export { SeparationsService } from './separations.service';
-export { SubmitSeparationDto } from './dto/submit-separation.dto';
 export * from './providers/separation-provider.types';
 export * from './providers/separation-provider.errors';
 export { StemSeparationProviderFactory } from './providers/stem-separation-provider.factory';
