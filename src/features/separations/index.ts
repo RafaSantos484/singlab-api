519530f530c0095b73ae05b7e68d87a0b79f9127
Rafael Santos de Jesus <rafael.jesus@anpd.gov.br>
2026-03-03 11:40:31 -0300

    refactor: update separations module exports

--- src/features/separations/index.ts
+++ src/features/separations/index.ts
@@ -1,6 +1,5 @@
 export { SeparationsModule } from './separations.module';
 export { SeparationsController } from './separations.controller';
-export { SeparationsService } from './separations.service';
-export * from './providers/poyo/poyo-separation.types';
+export { SeparationsProxyController } from './separations-proxy.controller';
 export { StemSeparationProviderFactory } from './providers/stem-separation-provider.factory';
 export { PoyoStemSeparationProvider } from './providers/poyo/poyo-separation.provider';
